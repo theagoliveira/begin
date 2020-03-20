267e60b4bc2f94fe498af4d4e8b746800e29fd90
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-20 11:25:49 -0300

    Add new config to backup_configs

--- backup_configs.sh
+++ backup_configs.sh
@@ -9,5 +9,5 @@ rm dolphin_configs.tar.gz
 
 tar -czf keyboard_shortcuts.tar.gz $(ls $(echo "$HOME"/.{config/{kdeglobals,kglobalshortcutsrc,khotkeysrc,plasma-org.kde.plasma.desktop-appletsrc,kwinrc},local/share/kxmlgui5/{katepart/katepart5,konsole/{konsole,session},kwrite/kwrite}ui.rc}) 2>/dev/null)
 tar -czf mimeapps_list.tar.gz $(ls $(echo {"$HOME"/{.config,.local/share/applications},/etc/xdg,/usr{/local,}/share/applications}/{kde-,}mimeapps.list) 2>/dev/null)
-tar -czf configs.tar.gz $(ls $(echo "$HOME"/{.pythonrc,.p10k.zsh,.zshrc,.config/{plasmarc,spotifyd/spotifyd.conf,terminator/config,sublime-text-3/Packages/User/SyncSettings.sublime-settings,mps-youtube/config},.local/share/konsole/{default.dark.colorscheme,Shell.profile}}) 2>/dev/null)
+tar -czf configs.tar.gz $(ls $(echo "$HOME"/{.oh-my-zsh/plugins/web-search/web-search.plugin.zsh,.pythonrc,.p10k.zsh,.zshrc,.config/{plasmarc,spotifyd/spotifyd.conf,terminator/config,sublime-text-3/Packages/User/SyncSettings.sublime-settings,mps-youtube/config},.local/share/konsole/{default.dark.colorscheme,Shell.profile}}) 2>/dev/null)
 tar -czf dolphin_configs.tar.gz $(ls $(echo "$HOME"/{.config/dolphinrc,.local/share/{kxmlgui5/dolphin/dolphinui.rc,user-places.xbel}}) 2>/dev/null)
