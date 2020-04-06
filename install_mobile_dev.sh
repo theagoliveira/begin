b8f7ccdd737bc4aa87e7354f6dec92e35bfbfa18
Thiago Cavalcante <thiago.kun@gmail.com>
2020-04-06 18:23:36 -0300

    Add script to install mobile dev tools (Flutter)

---
+++ install_mobile_dev.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+yay -S --noconfirm flutter
+yay -S --noconfirm dart
+yay -S --noconfirm android-sdk
+yay -S --noconfirm android-sdk-build-tools
+yay -S --noconfirm android-sdk-platform-tools
+yay -S --noconfirm android-platform
+yay -S --noconfirm android-ndk
+
+sudo groupadd flutterusers
+sudo gpasswd -a thiago flutterusers
+sudo chown -R :flutterusers /opt/flutter
+sudo chown -R :flutterusers /opt/android-sdk
+sudo chmod -R g+w /opt/flutter
+sudo chmod -R g+w /opt/android-sdk
+newgrp flutterusers
+flutter doctor -v
+
+$ANDROID_HOME/tools/bin/sdkmanager --licenses
+sudo $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+sudo touch /root/.android/repositories.cfg
+touch $HOME/.android/repositories.cfg
+
+flutter channel master
+flutter upgrade
